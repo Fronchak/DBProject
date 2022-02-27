73ca39fb221f02664660cee23f64a58a5b572dcc
Gabriel Fronchak <gabrielgmack@gmail.com>
2022-02-27 01:43:58 -0300

    Classes model.entities created

---
+++ src/Db/DB.java
@@ -0,0 +1,55 @@
+package Db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	public static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties prop = loadProperties();
+				String url = prop.getProperty("dburl");
+				conn = DriverManager.getConnection(url, prop);
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+		
+	}
+	
+	public void closeConnection() {
+		if(conn != null) {
+			
+			try {
+				conn.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	
+	private static Properties loadProperties() {
+		
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties prop = new Properties();
+			prop.load(fs);
+			return prop;
+			
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+}
---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.sql.Connection;
+
+import Db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
